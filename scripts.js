09e177a4bb8af2ad81e94fee24fde091ed9b51f6
Aleksandr Necheukhin <necheyhin_a_v@mail.ru>
2017-06-20 17:11:48 +0700

    Added styles, added js function (markAsChecked) that mark element in the list as checked. ~5 hours

--- scripts.js
+++ scripts.js
@@ -5,14 +5,15 @@
 
 function addNewItem(event, element) {
     //check for the pressed key and empty string
-    if(event.keyCode === 13 && element !== ""){
+    if(event.keyCode === 13 && element.value !== ""){
         //AJAX technology
         // code for modern browsers (IE 6+)
         var xmlhttp = new XMLHttpRequest();
         //Define the function to be called when the state changes
         xmlhttp.onreadystatechange = function() {
             if (this.readyState === 4 && this.status === 200) { //4: request finished and response is ready, 200 - status OK
-                addItemToList(element);
+                addItemToList(element.value);
+                element.value = ""; //clear element in success
             }
         };
         xmlhttp.open("GET", "index.html", true);
@@ -27,6 +28,18 @@ function addItemToList(itemName)
 {
     var newItem = document.createElement("li");
     newItem.innerHTML = itemName;
-
-    document.getElementById("taskList").appendChild(newItem);
+    newItem.onclick = function () {
+        markAsChecked(newItem);
+    };
+    document.getElementById("listItem").appendChild(newItem);
+}
+function markAsChecked(item) {
+    if (item.classList) {
+        if(item.classList.contains("checked"))
+            item.classList.remove("checked");
+        else
+            item.classList.add("checked");
+    } else {
+        //old browser
+    }
 }

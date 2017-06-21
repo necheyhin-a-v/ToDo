e7074b22a77f05b927ab3bfdb22a882939a725e8
Aleksandr Necheukhin <necheyhin_a_v@mail.ru>
2017-06-21 15:45:06 +0700

    Added remove items feature and corresponding behaviour with styles. ~3 hours

--- scripts.js
+++ scripts.js
@@ -2,17 +2,18 @@
  * Created by Aleksandr Necheukhin on 18.06.2017.
  */
 
+var xmlhttp;
 
 function addNewItem(event, element) {
     //check for the pressed key and empty string
     if(event.keyCode === 13 && element.value !== ""){
         //AJAX technology
         // code for modern browsers (IE 6+)
-        var xmlhttp = new XMLHttpRequest();
+        xmlhttp = new XMLHttpRequest();
         //Define the function to be called when the state changes
         xmlhttp.onreadystatechange = function() {
             if (this.readyState === 4 && this.status === 200) { //4: request finished and response is ready, 200 - status OK
-                addItemToList(element.value);
+                addItemToList(element);
                 element.value = ""; //clear element in success
             }
         };
@@ -24,22 +25,69 @@ function addNewItem(event, element) {
     }
 
 }
-function addItemToList(itemName)
-{
+
+function addItemToList(element) {
+    //label
+    var span = document.createElement("span");
+    span.innerHTML = element.value;
+    //close button
+    var closeButton = document.createElement("button");
+    closeButton.className = "close hidden";
+    closeButton.innerHTML = "x";
+    closeButton.onclick = function() {
+        removeItemFromList(closeButton);
+    };
+    //li element
     var newItem = document.createElement("li");
-    newItem.innerHTML = itemName;
     newItem.onclick = function () {
         markAsChecked(newItem);
     };
+    newItem.onmouseover = function () {
+        mouseOver(newItem);
+    };
+    newItem.onmouseout = function () {
+        mouseOut(newItem);
+    };
+    newItem.appendChild(span);
+    newItem.appendChild(closeButton);
+    //put created item to the right position
     document.getElementById("listItem").appendChild(newItem);
 }
-function markAsChecked(item) {
-    if (item.classList) {
-        if(item.classList.contains("checked"))
-            item.classList.remove("checked");
+
+function removeItemFromList(element) {
+    event.stopPropagation();            /*stop sending event to parent node*/
+    var li = element.parentNode;
+    var ul = li.parentNode;
+    ul.removeChild(li);
+}
+
+function markAsChecked(element) {
+    if (element.classList) {
+        if(element.classList.contains("checked"))
+            element.classList.remove("checked");
         else
-            item.classList.add("checked");
+            element.classList.add("checked");
     } else {
         //old browser
     }
+}
+
+function mouseOver(element) {
+    element = element.lastElementChild;
+    if (element.classList) {
+        if(element.classList.contains("hidden"))
+            element.classList.remove("hidden");
+    } else {
+        //old browser
+    }
+}
+
+function mouseOut(element) {
+    element = element.lastElementChild;
+    if(!element.classList.contains("hidden"))
+        element.classList.add("hidden");
+    else {
+        //old browser
+    }
+
 }
